ec007055d2064300743d03c21c9af8b3d959bae4
galaxyspacean <lancanglss@163.com>
2023-01-19 20:48:41 +0800

    勿用

--- 2048/ui.cpp
+++ 2048/ui.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
-void show(int map)
+void show()
 {
-    cout<<"======================================="<<endl;
     cout<<"您当前的分数为"<<score<<"分"<<endl;
-    cout<<"======================================="<<endl;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<" ";
+        }
+    }
 }
