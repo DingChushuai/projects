efd58accb014c6b5d9ef713fcc4db9959d8e94e5
DingChushuai <1716633584@qq.com>
2023-01-19 20:49:46 +0800

    3

--- 2048/begin.cpp
+++ 2048/begin.cpp
@@ -1,4 +1,8 @@
-extern map;
+#include<conio.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+extern int map[4][4];
 extern score;
 
 void add();
@@ -9,13 +13,42 @@ void begin()
     score=0;
     while (1)
     {
-        int right=0;
+        int right=0,ifend=0;
+        char direction='\0';
         show(map);
         do
         {
-            /* code */
-        } while (right);
-        
+            direction=getch();
+            if (direction=='w'||direction=='a'||direction=='s'||direction=='d') right=1;
+        } while (!right);
+        ifend=update(direction,map);
+        if (ifend==1) 
+        {
+            system("cls");
+            printf("游戏失败!\n")
+        }
+        if (ifend==2) 
+        {
+            system("cls");
+            printf("游戏成功!\n")
+        }
+        if (ifend)
+        {
+            printf("您的最终分数是:%d\n",score);
+            char re;
+            printf("是否再来一局?(y/n)\n");
+            re=getchar();
+            if(re=='y')
+            {
+                score=0;
+                for(i=0;i<16;i++) map[0][i]=0;
+                continue;
+            }
+            else
+            {
+                exit(0);
+            }
+        }
     }
     
 }
