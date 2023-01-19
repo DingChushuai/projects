0aa8735d98dbaa56189f95df427dcd170a5a4d75
DingChushuai <1716633584@qq.com>
2023-01-19 21:00:07 +0800

    4

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -4,9 +4,9 @@ int map[4][4]={0};
 int score=0;
 
 void begin();//0显示空,是否重新开始
-void add(int *map[4]);
-int update(char direction,int *map[4]);//失败返回1,合成出2048返回2
-void show(int *map[4]);//分数显示在第一行,界面方形
+void add(int map[][4]);
+int update(char direction,int map[][4]);//失败返回1,合成出2048返回2
+void show(int map[][4]);//分数显示在第一行,界面方形
 
 int main()
 {
