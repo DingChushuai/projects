bc9c7fd52dc2f8a0e1dbbd1e5e2557aee2a0cef8
galaxyspacean <lancanglss@163.com>
2023-01-19 21:08:15 +0800

    finish

--- 2048/ui.cpp
+++ 2048/ui.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
-void show()
+void show(int *map[])
 {
     cout<<"您当前的分数为"<<score<<"分"<<endl;
     for(int i=0;i<4;i++){
+        cout<<"====================="<<endl;
+        cout<<"|";
         for(int j=0;j<4;j++){
-            cout<<" ";
+            cout<<setw(4)<<setfill(' ')<<map[i][j]<<"|";
         }
+        cout<<endl;
     }
+    cout<<"====================="<<endl;
+}
+int main()
+{
+    show(map);
 }
