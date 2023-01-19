de52bfa6a07404a36cfa486fbcf9fc4277e044e7
galaxyspacean <lancanglss@163.com>
2023-01-19 20:39:14 +0800

    score

---
+++ 2048/ui.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+void show(int map)
+{
+    cout<<"======================================="<<endl;
+    cout<<"您当前的分数为"<<score<<"分"<<endl;
+    cout<<"======================================="<<endl;
+}
