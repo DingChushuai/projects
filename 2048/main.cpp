b59ee9e97c0479762e3d0254b9370adafc294a4c
DingChushuai <1716633584@qq.com>
2023-01-19 20:21:09 +0800

    2

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
-void menu();
+int map[4][4]={0};
+int score=0;
+
+void begin();//0显示空,是否重新开始
+int update(char direction,int map);//失败返回1,合成出2048返回2
+void show(int map);//分数显示在第一行,界面方形
 
 int main()
 {
-    while(1) menu();
+    while(1) begin();
 }
